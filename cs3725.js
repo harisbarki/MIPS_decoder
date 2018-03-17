7432a65eb1f6ed9007dd1625e9582a324aaf1a9a
Tomisin <tomisinjenrola@gmail.com>
2018-03-17 12:35:48 -0230

    Split command

--- cs3725.js
+++ cs3725.js
@@ -1,6 +1,33 @@
 var MIPS = function (command) {
     var self = {};
-    self.command = command;
+    self.command = command.split(" ");
+    console.log(self.command);
+
+    self.opcode = self.command[0];
+    self.op1 = self.command[1];
+    self.op2 = self.command[2];
+    self.op3 = self.command[3];
+
+    console.log(self.opcode);
+    console.log(self.op1);
+    console.log(self.op2);
+    console.log(self.op3);
+
+    self.instructionFetch = function () {
+
+    }
+
+    self.instructionDecode = function () {
+
+    }
+
+    self.execute = function() {
+
+    }
+
+    self.memory = function () {
+
+    }
 
     return self;
 };
